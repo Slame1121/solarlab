abf1bae9ba7dabd943b4c0cee3e655654dfe9ccc
Oleg <slame1121@gmail.com>
2018-01-07 16:11:28 +0200

    big text section

--- js/pages/home.js
+++ js/pages/home.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
 		animationTime: 1000,
 		showTime: 4000
 	});
+	$('#slider_2').carousel({
+		num: 3,
+		maxWidth: 665,
+		maxHeight: 460,
+		distance: 100,
+		scale: 0.6,
+		autoPlay: false,
+		animationTime: 1000,
+		showTime: 4000
+	});
 });
