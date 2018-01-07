5ada6e94eca9785f3b7779a5a748109c0e061a4d
Oleg <slame1121@gmail.com>
2018-01-07 15:05:37 +0200

    description section

---
+++ js/pages/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#slider_1').carousel({
+		num: 3,
+		maxWidth: 665,
+		maxHeight: 460,
+		distance: 100,
+		scale: 0.6,
+		autoPlay: false,
+		animationTime: 1000,
+		showTime: 4000
+	});
+});
