93d5e8d9962931daf416aae182d79cf598ca6bb3
Oleg <slame1121@gmail.com>
2018-01-09 00:28:13 +0200

    responsive and fixed header

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,6 +2,45 @@ $(document).ready(function(){
 
 	var Home = {
 		setSliders: function () {
+			var owl = $('#reviews_carousel');
+			owl.owlCarousel({
+				margin: 10,
+				nav: true,
+				dots:true,
+				loop: true,
+				navText: ["<img src='images/left-arrow-white.png' />", "<img src='images/right-arrow-white.png' />"],
+				responsive: {
+					0: {
+						items: 1
+					},
+					600: {
+						items: 1
+					},
+					1000: {
+						items: 1
+					}
+				}
+			});
+			owl = $('#partners_slider');
+			owl.owlCarousel({
+				margin: 10,
+				nav: true,
+				loop: true,
+				items:3,
+				navText: ["<img src='images/left-arrow.png' />", "<img src='images/right-arrow.png' />"],
+				responsive: {
+					0: {
+						items: 1
+					},
+					600: {
+						items: 1
+					},
+					1000: {
+						items: 1
+					}
+				}
+			});
+
 			var sliders_sizes = {
 				maxWidth: 665,
 				maxHeight: 460
@@ -21,8 +60,8 @@ $(document).ready(function(){
 				maxHeight: sliders_sizes['maxHeight'],
 				distance: distance,
 				scale: 0.6,
-				autoPlay: false,
-				animationTime: 1000,
+				autoPlay: true,
+				animationTime: 500,
 				showTime: 4000
 			});
 
@@ -32,54 +71,36 @@ $(document).ready(function(){
 				maxHeight: sliders_sizes['maxHeight'],
 				distance: distance,
 				scale: 0.6,
-				autoPlay: false,
-				animationTime: 1000,
+				autoPlay: true,
+				animationTime: 500,
 				showTime: 4000
+			});
+		},
+		initHeaderMenu: function () {
+			//responsive
+			$('.headblock-item_nav-menutoggler').on('click', function(){
+				$('.response_dropdown_menu').slideToggle('fast');
+			});
+
+			//fixed
+			$(window).bind('scroll', function () {
+				if ($(window).scrollTop() > 0) {
+					$('#header').addClass('fixed');
+					$('#banner').addClass('fixed');
+
+				} else {
+					$('#banner').removeClass('fixed');
+					$('#header').removeClass('fixed');
+				}
 			});
 		},
 		init: function(){
 			this.setSliders();
+			this.initHeaderMenu();
 		}
 	};
 	Home.init();
 
 
-	var owl = $('#reviews_carousel');
-	owl.owlCarousel({
-		margin: 10,
-		nav: true,
-		dots:true,
-		loop: true,
-		navText: ["<img src='images/left-arrow-white.png' />", "<img src='images/right-arrow-white.png' />"],
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 1
-			},
-			1000: {
-				items: 1
-			}
-		}
-	});
-	owl = $('#partners_slider');
-	owl.owlCarousel({
-		margin: 10,
-		nav: true,
-		loop: true,
-		items:3,
-		navText: ["<img src='images/left-arrow.png' />", "<img src='images/right-arrow.png' />"],
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 3
-			},
-			1000: {
-				items: 3
-			}
-		}
-	});
+
 });
