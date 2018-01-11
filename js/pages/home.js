5c8cf16668eb7a181c2e67a8cd35146eabf2da57
Oleg <slame1121@gmail.com>
2018-01-11 23:39:23 +0200

    +2 pages and starting 3 page

--- js/pages/home.js
+++ js/pages/home.js
@@ -85,26 +85,14 @@ $(document).ready(function(){
 			});
 		},
 		initHeaderMenu: function () {
-			//responsive
-			$('.headblock-item_nav-menutoggler').on('click', function(){
-				$('.response_dropdown_menu').slideToggle('fast');
-				if($('#header').css('background-color') == 'rgb(0, 0, 0)'){
-					$('#header').css('background-color','rgba(4, 15, 22, 0.7)');
-				}else{
-					$('#header').css('background-color','#000');
-				}
-
-			});
 
 			//fixed
 			$(window).bind('scroll', function () {
 				if ($(window).scrollTop() > 0) {
-					$('#header').addClass('fixed');
 					$('#banner').addClass('fixed');
 
 				} else {
 					$('#banner').removeClass('fixed');
-					$('#header').removeClass('fixed');
 				}
 			});
 		},
---
+++ js/pages/why_it_the_best.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	var Why_it_the_best = {
+		setSliders: function () {
+
+			var owl = $('#our_command_carousel');
+			owl.owlCarousel({
+				margin: 10,
+				nav: true,
+				loop: true,
+				items:3,
+				navText: ["<img src='images/left-arrow.png' />", "<img src='images/right-arrow.png' />"],
+				responsive: {
+					0: {
+						items: 1
+					},
+					600: {
+						items: 2
+					},
+					1000: {
+						items: 3
+					}
+				}
+			});
+
+			owl = $('#partners_slider');
+			owl.owlCarousel({
+				margin: 10,
+				nav: true,
+				loop: true,
+				items:3,
+				navText: ["<img src='images/left-arrow.png' />", "<img src='images/right-arrow.png' />"],
+				responsive: {
+					0: {
+						items: 1
+					},
+					600: {
+						items: 2
+					},
+					1000: {
+						items: 3
+					}
+				}
+			});
+		},
+
+		init: function(){
+			this.setSliders();
+		}
+	};
+	Why_it_the_best.init();
+
+
+
+});
